4662b0e237304500bad4000f3af311b7345e5c2e
DaniellaJonas <daniella.jonas11@gmail.com>
2025-06-19 18:31:41 +0300

    flasking

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,14 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
 import matplotlib.pyplot as plt 
-from flask import Flask
 
-app = Flask(__name__)
+from flask import Flask, render_template
 
-@app.route("/")
+app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
+
+@app.route('/')
 def home():
-    return "Welcome to our first Flask website!"
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)

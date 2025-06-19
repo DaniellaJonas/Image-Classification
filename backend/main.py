5c5dd73e9997d76c4bbaf0746f8edd5a88ecab8e
DaniellaJonas <daniella.jonas11@gmail.com>
2025-06-19 18:27:00 +0300

    flask

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,13 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
 import matplotlib.pyplot as plt 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to our first Flask website!"
+
+if __name__ == "__main__":
+    app.run(debug=True)

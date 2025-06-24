6616dbc7c71669fd7a3af6fe51494d9687abada8
DaniellaJonas <daniella.jonas11@gmail.com>
2025-06-24 22:18:50 +0300

    py

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,18 @@ import matplotlib.pyplot as plt
 from PIL import Image 
 from sklearn.metrics import confusion_matrix, classification_report
 
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Rescaling(1./255, input_shape=(1200, 1800, 3)),
+    tf.keras.layers.Conv2D((3,3),32, activation="relu"),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+
+])
+
+model.compile(
+    loss="binary_crossentropy",
+    optimizer="adam",
+    metrics=["accuracy"]
+)

0b9e87e54a49f2d16d5972566a3942f317c4a061
DaniellaJonas <daniella.jonas11@gmail.com>
2025-06-24 23:29:35 +0300

    image upload

--- frontend/home.js
+++ frontend/home.js
@@ -2,3 +2,16 @@ function upload(){
     document.getElementById('fileInput').click();
     console.log("It's working")
 }
+
+function img_load(event){
+   const file = event.target.files[0];
+    if (!file || (file.type !== "image/jpeg" && file.type !== "image/png")) {
+        return;
+    } 
+    const img = document.getElementById("preview");
+    img.src = URL.createObjectURL(file);  
+    img.onload = () => {
+        URL.revokeObjectURL(img.src); 
+    }
+}
+

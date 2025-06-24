8a8fc1a0121d13c757516abfb4abce0a6f49b851
DaniellaJonas <daniella.jonas11@gmail.com>
2025-06-24 23:34:58 +0300

    image upload problems

--- frontend/home.js
+++ frontend/home.js
@@ -6,12 +6,16 @@ function upload(){
 function img_load(event){
    const file = event.target.files[0];
     if (!file || (file.type !== "image/jpeg" && file.type !== "image/png")) {
+        console.log("Not image"),
+        alert("Upload a PNG/JPG")
         return;
     } 
     const img = document.getElementById("preview");
     img.src = URL.createObjectURL(file);  
     img.onload = () => {
         URL.revokeObjectURL(img.src); 
+        img.style.display = "block";
+
     }
 }
 

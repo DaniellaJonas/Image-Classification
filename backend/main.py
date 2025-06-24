5f46aea620c58d99902f52da6197307d62a99e5f
DaniellaJonas <daniella.jonas11@gmail.com>
2025-06-24 21:40:07 +0300

    changes

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,6 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
 import matplotlib.pyplot as plt 
-from flask import Flask, render_template
+from PIL import Image 
+from sklearn.metrics import confusion_matrix, classification_report
 
-app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)

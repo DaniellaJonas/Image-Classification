1558d7d24b7458455177c9093e98942b2dbaf48f
DaniellaJonas <daniella.jonas11@gmail.com>
2025-07-02 15:32:33 +0300

    f

--- frontend/home.js
+++ frontend/home.js
@@ -13,27 +13,41 @@ function img_load(event) {
     const img = document.getElementById("preview");
     img.src = URL.createObjectURL(file);
 
-    img.onload = () => {
-        URL.revokeObjectURL(img.src);
+    img.onload = () => { // טוענת תמונה
+        URL.revokeObjectURL(img.src); 
+        // מוחקת תמונה מהזיכרון
         img.style.display = "block";
+        // מציגה אותה בפרונטאנד
+
     };
 
+
+    // יוצרת תיקייה לתמונה
     const formData = new FormData();
     formData.append("file", file);
 
+
+    // שולחים לפלסק את התמונה לבדיקה
     fetch("http://localhost:5000/predict", { 
         method: "POST",
         body: formData
     })
+
+    // מקבל תשובה ואם שגיאה מחזיר שגיאה
     .then(response => {
         if (!response.ok) {
-            throw new Error("HTTP status " + response.status);
+            // throw new Error("HTTP status " + response.status)
+            alert("Error");
         }
         return response.text();
     })
+
+    // מדפיס תשובה של הדאטה
     .then(data => {
         alert("Prediction: " + data);
     })
+
+    //בודקים איזה שגיאה התקבלה מthen
     .catch(error => {
         console.error("Error:", error);
         alert("Something went wrong: " + error.message);
